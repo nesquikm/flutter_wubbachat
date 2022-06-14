5b2bb5f52894357516c2944c6b0fc15151bd25d3
nesquikm <drapolyuk@gmail.com>
2022-06-14 17:39:31 +0400

    Changed showAddChatDialog to use custom StatefulWidget

--- lib/pages/chats/view/add_chat_dialog.dart
+++ lib/pages/chats/view/add_chat_dialog.dart
@@ -1,86 +1,119 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_wubbachat/l10n/l10n.dart';
 
-final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-final TextEditingController _textEditingController = TextEditingController();
+class _AddChatDialog extends StatefulWidget {
+  const _AddChatDialog();
 
-Future<String?> showAddChatDialog(BuildContext context) {
-  final l10n = context.l10n;
+  @override
+  State<_AddChatDialog> createState() => _AddChatDialogState();
+}
 
-  _textEditingController.clear();
-  var createNew = false;
+class _AddChatDialogState extends State<_AddChatDialog> {
+  late final GlobalKey<FormState> _formKey;
+  late final TextEditingController _textEditingController;
+  late final FocusNode _textEditingFocusNode;
+  bool _createNew = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _formKey = GlobalKey<FormState>();
+    _textEditingController = TextEditingController();
+    _textEditingFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    _textEditingFocusNode.dispose();
+
+    super.dispose();
+  }
 
   void _onAdd() {
-    Navigator.pop(context, createNew ? '' : _textEditingController.text);
+    Navigator.pop(context, _createNew ? '' : _textEditingController.text);
   }
 
   void _onCancel() {
     Navigator.pop(context);
   }
 
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return AlertDialog(
+      title: Text(l10n.dialogTitleNewChat),
+      content: Form(
+        onChanged: () {
+          setState(() {});
+        },
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              autofocus: true,
+              focusNode: _textEditingFocusNode,
+              decoration: InputDecoration(
+                hintText: _createNew
+                    ? l10n.dialogTextHintCreateNew
+                    : l10n.dialogTextHintJoinExisting,
+              ),
+              controller: _textEditingController,
+              onFieldSubmitted: (_) => _onAdd,
+              enabled: !_createNew,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(l10n.dialogCheckboxCreateNewChat),
+                Checkbox(
+                  value: _createNew,
+                  onChanged: (checked) {
+                    setState(() {
+                      _createNew = checked == true;
+                      if (_createNew) {
+                        _textEditingController.clear();
+                      }
+                    });
+                    if (!_createNew) {
+                      WidgetsBinding.instance.addPostFrameCallback(
+                        (timeStamp) => _textEditingFocusNode.requestFocus(),
+                      );
+                    }
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: _onCancel,
+          child: Text(l10n.dialogActionCancel),
+        ),
+        TextButton(
+          onPressed: (_createNew || _textEditingController.text.isNotEmpty)
+              ? _onAdd
+              : null,
+          child: Text(
+            _createNew
+                ? l10n.dialogActionCreateNewChat
+                : l10n.dialogActionJoinExistingChat,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+Future<String?> showAddChatDialog(BuildContext context) {
   return showDialog<String>(
     context: context,
     builder: (context) {
-      return StatefulBuilder(
-        builder: (context, setState) {
-          return AlertDialog(
-            title: Text(l10n.dialogTitleNewChat),
-            content: Form(
-              onChanged: () {
-                setState(() {});
-              },
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextFormField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                      hintText: createNew
-                          ? l10n.dialogTextHintCreateNew
-                          : l10n.dialogTextHintJoinExisting,
-                    ),
-                    controller: _textEditingController,
-                    onFieldSubmitted: (_) => _onAdd,
-                    enabled: !createNew,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(l10n.dialogCheckboxCreateNewChat),
-                      Checkbox(
-                        value: createNew,
-                        onChanged: (checked) {
-                          setState(() {
-                            createNew = checked == true;
-                            _textEditingController.clear();
-                          });
-                        },
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-            actions: [
-              TextButton(
-                onPressed: _onCancel,
-                child: Text(l10n.dialogActionCancel),
-              ),
-              TextButton(
-                onPressed: (createNew || _textEditingController.text.isNotEmpty)
-                    ? _onAdd
-                    : null,
-                child: Text(
-                  createNew
-                      ? l10n.dialogActionCreateNewChat
-                      : l10n.dialogActionJoinExistingChat,
-                ),
-              ),
-            ],
-          );
-        },
-      );
+      return const _AddChatDialog();
     },
   );
 }
